be65bfaacdbb3775221bfcedb87e6fc5c3fd991a
markbello <mcbello51286@gmail.com>
2018-01-29 15:16:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(arr, str){
+  return arr.filter(function(item){
+    return item.toLowerCase() === str.toLowerCase()
+  })
+}
+
+function fuzzyMatch(arr, str){
+  return arr.filter(function(item){
+    return item.startsWith(str)
+  })
+}
+
+function matchName(arr, str){
+  return arr.filter(function(item){
+    return item.name === str
+  })
+}
